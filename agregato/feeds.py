30c4702ed8a16b2daecc86896462c281eb22f99e
Rafał Selewońko <rafal@selewonko.com>
2018-01-12 01:05:48 +0100

    Feed for one watch.'

--- agregato/feeds.py
+++ agregato/feeds.py
@@ -1,11 +1,11 @@
 from django.contrib.syndication.views import Feed
-from agregato.models import Item
+from agregato.models import Item, Watch
 
 
-class LatestEntriesFeed(Feed):
+class LatestItemsFeed(Feed):
     title = "All Items"
-    link = "/sitenews/"
     description = "All Recent Items"
+    link = '/cipa/'
 
     def items(self):
         return Item.objects.order_by('-add_date')[:10]
@@ -14,7 +14,19 @@ class LatestEntriesFeed(Feed):
         return item.title
 
     def item_description(self, item):
-        return item.content
+        return item.content[:255] + '...'
 
     def item_link(self, item):
         return item.href
+
+
+class WatchItemsFeed(LatestItemsFeed):
+    title = "Watch Items"
+    description = "Watch Recent Items"
+    link = '/dupa/'
+
+    def get_object(self, request, watch_id):
+        return Watch.objects.get(pk=watch_id)
+
+    def items(self, watch):
+        return Item.objects.filter(watch=watch).order_by('-add_date')[:10]
--- agregato/urls.py
+++ agregato/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from agregato.feeds import LatestEntriesFeed
+from agregato.feeds import LatestItemsFeed, WatchItemsFeed
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('^feed/latest/', LatestEntriesFeed()),
+    url('^feeds/latest/$', LatestItemsFeed()),
+    path('feeds/<int:watch_id>/', WatchItemsFeed()),
 ]
